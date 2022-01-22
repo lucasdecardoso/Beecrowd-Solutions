bd2aa59aa8b8b64c8d52d94a72f84d85f7bfae8e
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2022-01-22 01:06:09 -0300

    Create 1078.c

---
+++ Iniciante/1078.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int N,i,resultado = 0;
+
+    scanf("%d", &N);
+
+
+    for(i = 1; i <= 10; i++)
+    {
+        {
+            printf("%d x %d = %d\n",i,N, resultado = i * N);
+        }
+
+
+    }
+
+
+
+
+
+    return 0;
+
+}
