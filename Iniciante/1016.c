e2a59b2d3854a36e2e44dc130c7ed30d1bd4ab46
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2022-01-22 00:38:06 -0300

    Create 1016.c

---
+++ Iniciante/1016.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+    int dist;
+
+
+    scanf("%d", &dist);
+
+    dist = dist *2;
+
+    printf("%d minutos\n", dist);
+
+
+
+    return 0;
+}
