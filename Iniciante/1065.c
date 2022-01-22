b84ab21ea313a65754caa7a0e08964037374f12d
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2022-01-22 01:02:14 -0300

    Create 1065.c

---
+++ Iniciante/1065.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int N,i, par = 0;
+
+
+    for(i = 0; i < 5; i++)
+    {
+
+        scanf("%d", &N);
+
+    {
+        if (N  % 2 == 0 )
+        {
+            par = par + 1;
+        }
+
+    }
+
+
+
+    }
+
+    printf("%d valores pares\n",par);
+
+
+
+
+
+    return 0;
+
+}
