ba49a3f5e67ae4b695be85dd7ecdf7164a8515be
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2022-01-23 23:47:06 -0300

    Update and rename refazer1047.c to 1047.c

---
+++ Iniciante/1047.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    int horaInicial, minutoInicial, horaFinal, minutoFinal;
+    
+    scanf("%d %d %d %d", &horaInicial, &minutoInicial, &horaFinal, &minutoFinal);
+    
+    horaInicial = horaInicial * 60;
+    horaFinal = horaFinal * 60;
+    minutoInicial =  minutoInicial + horaInicial;
+    minutoFinal = minutoFinal + horaFinal;
+    
+    if (minutoFinal <= minutoInicial)
+        minutoFinal = minutoFinal + 1440;
+    
+    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", (minutoFinal-minutoInicial)/60, (minutoFinal-minutoInicial)%60);
+    
+    return 0;
+}
