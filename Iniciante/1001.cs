cf353ca747d75be26913b7eb30bf1e5e8079ce1c
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2023-02-22 22:52:45 -0300

    Update 1001.cs

--- Iniciante/1001.cs
+++ Iniciante/1001.cs
@@ -1,4 +1,6 @@
 /*
+beecrowd | 1001 - Extremamente Básico
+
 Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
 Efetue a soma de A e B atribuindo o seu resultado na variável X. 
 Imprima X conforme exemplo apresentado abaixo. 
