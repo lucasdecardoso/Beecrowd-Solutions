cf811db7e47f6e0776152b52d428885f17370ac8
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2023-04-03 22:48:10 -0300

    Create 1132.cs

---
+++ Iniciante/C#/1132.cs
@@ -0,0 +1,40 @@
+/*
+beecrowd | 1132 - Múltiplos de 13
+
+Escreva um algoritmo que leia 2 valores inteiros X e Y calcule a soma dos números que não são múltiplos de 13 entre X e Y, incluindo ambos.
+
+Entrada: O arquivo de entrada contém 2 valores inteiros quaisquer, não necessariamente em ordem crescente.
+
+Saída: Imprima a soma de todos os valores não divisíveis por 13 entre os dois valores lidos na entrada, inclusive ambos se for o caso.
+*/
+
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int x, y, maior, somaTotal = 0;
+
+        x = int.Parse(Console.ReadLine());
+        y = int.Parse(Console.ReadLine());
+
+        if (x > y)
+        {
+            maior = x;
+            x = y;
+            y = maior;
+        }
+
+        for (int i = x; i <= y; i++)
+        {
+            if (i % 13 != 0)
+            {
+                somaTotal = somaTotal + i;
+            }
+        }
+
+        Console.WriteLine(somaTotal);
+    }
+}
