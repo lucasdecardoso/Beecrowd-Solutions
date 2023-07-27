8bc27ff35640ff4195a650f9d34110d0050f5387
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2023-07-26 22:17:20 -0300

    Create 1004.java

---
+++ Iniciante/Java/1004.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        scanner.close();
+
+        int total = x * y;
+
+        System.out.println("PROD = " + total);
+    }
+}
