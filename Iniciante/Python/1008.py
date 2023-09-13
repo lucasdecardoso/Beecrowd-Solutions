7a95ef3d245fc9116d56b1a9007c62bd9460de39
Lucas Cardoso <lucas_cardoso_de_castro@hotmail.com>
2023-09-13 18:34:33 -0300

    Create 1008.py

---
+++ Iniciante/Python/1008.py
@@ -0,0 +1,8 @@
+#beecrowd | 1008 - Salário
+
+numeroFuncionario = int(input())
+horasTrabalhadas = int(input())
+salario = float(input())
+
+print("NUMBER =", numeroFuncionario)
+print("SALARY = U${: .2f}".format(salario * horasTrabalhadas))
